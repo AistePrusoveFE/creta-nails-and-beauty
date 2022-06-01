6a59c15994987a50d03b98c67d3b8a900868202e
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-06-01 16:45:40 +0300

    simple styling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import background from './assets/background.jpeg'
+import IMAGES from './images';
+
 function App() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center text-5xl bg-[#ff0088] text-white">Creta Nails and Beauty</div>
+    <div className="pb-20">
+       <div className="mb-16">
+            <img src={background} alt="leaves and name" className="h-96 w-full object-cover"/>
+       </div>
+       <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-screen-md mx-auto px-12">
+           {IMAGES.map((image, index) => {
+               return (
+                   <div className="h-48">
+                       <img key={index} src={image} alt="nails design" className="h-full w-full object-cover"/>
+                   </div>
+               )
+           })}
+       </div>
+    </div>
   );
 }
 
